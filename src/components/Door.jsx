1b794f5defd8e52c6f8e9ed95f489bd5abe198fd
nizar7zak <nizar7zakout@gmail.com>
2023-06-01 12:01:01 +0200

    remove position and scale

--- src/components/Door.jsx
+++ src/components/Door.jsx
@@ -2,12 +2,10 @@ import { useGLTF } from "@react-three/drei"
 
 const Door = () => {
     const door = useGLTF('./door.glb')
-    
+
     return (
         <primitive
             object={door.scene}
-            scale={0.01}
-            position={[0, -0.5, -2]}
         />
     )
 }
