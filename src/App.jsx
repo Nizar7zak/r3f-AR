ed3da8d2374061414bfedd2db78dc83c4e0727fe
nizar7zak <nizar7zakout@gmail.com>
2023-06-01 13:50:21 +0200

    delete AR useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,11 @@ import Fox from './components/Fox'
 import Door from './components/Door'
 import Room from './components/Room'
 import './index.css'
-import { useEffect } from 'react'
 
 
 
 export default function App() {
 
-  useEffect(() => {
-    // Check for AR support
-    if ('xr' in navigator && 'AR' in navigator.xr) {
-      // Request AR session
-      navigator.xr.requestSession('immersive-ar', {
-        requiredFeatures: ['hit-test'],
-      }).then((session) => {
-        // AR session is available
-        // Perform further operations in the AR session
-      }).catch((error) => {
-        // Failed to request AR session
-        console.error('Failed to request AR session:', error);
-      });
-    } else {
-      // AR not supported
-      console.error('AR not supported on this device');
-    }
-  }, []);
-  
   return (
     <>
       <ARButton />
