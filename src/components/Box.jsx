33201829512c7d3ea8ce29553a860762ee2e8045
nizar7zak <nizar7zakout@gmail.com>
2023-06-01 11:50:49 +0200

    remove useHitTest

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,14 +1,6 @@
-import { useHitTest } from "@react-three/xr"
-import { useRef } from "react"
-
 export default function Box({ color, size, scale, children, ...rest }) {
-    const ref = useRef()
-    useHitTest((hit) => {
-        hit.decompose(ref.current.position, ref.current.rotation, ref.current.scale)
-    })
-
     return (
-        <mesh ref={ref} scale={scale} {...rest}>
+        <mesh scale={scale} {...rest}>
             <boxGeometry args={size} />
             <meshPhongMaterial color={color} />
             {children}
